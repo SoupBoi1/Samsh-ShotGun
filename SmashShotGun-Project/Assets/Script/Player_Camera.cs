1fb5d17a76bf14c7a70b6f59f2ccb4599665aa81
soupduH <sudipta.halder@hotmail.com>
2021-01-01 11:26:59 -0600

    new input, Gravity & jump
    sopport controll and keybard input and play can jump, and fall

--- SmashShotGun-Project/Assets/Script/Player_Camera.cs
+++ SmashShotGun-Project/Assets/Script/Player_Camera.cs
@@ -1,38 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Camera : MonoBehaviour
 {
-    float x;
-    float y;
-    float xRotation;
+    
 
     public float xSen = 180f;
     public float ySen = 100f;
     public Transform orgn;
+
+    
+    float x;
+    float y;
+    float xRotation;
+    Vector2 LookD;
+
+    public InputActionAsset actionsAssests;
+    InputActionMap onFootMap;
+    InputAction lookAction;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+
+        onFootMap = actionsAssests.FindActionMap("OnFoot");
+
+        onFootMap.Enable();
+
+        lookAction = onFootMap.FindAction("look");
+
+        lookAction.performed += context => OnLook(context);
+        lookAction.canceled += canx => OnLook(canx);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Mouse X") * xSen * Time.deltaTime;
-        y = Input.GetAxis("Mouse Y") * ySen * Time.deltaTime;
+        Look(LookD);
+    }
 
-        Mathf.Clamp(y, 0, 90);
+    void OnLook(InputAction.CallbackContext context)
+    {
+        LookD = context.ReadValue<Vector2>();
+    }
+
+    void Look(Vector2 L)
+    {
+        x = L.x * xSen * Time.deltaTime;
+        y = L.y * ySen * Time.deltaTime;
+
+        
         xRotation -= y;
-
-
-
-
-
+        Mathf.Clamp(xRotation, 0, 90);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         //transform.Rotate(Vector3.right * y);
-        orgn.Rotate(Vector3.up*x);
-        
+        orgn.Rotate(Vector3.up * x);
     }
+
 }
--- SmashShotGun-Project/Assets/Script/Player_Movement.cs
+++ SmashShotGun-Project/Assets/Script/Player_Movement.cs
@@ -1,30 +1,130 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Player_Movement : MonoBehaviour
 {
 
     public CharacterController CharacterController;
+    public Transform groundCheck;
     public Transform CameraT;
-    public float speed;
+    public float speed = 12f;
+    public bool Jumpable;
+    public float JumpHieght = 5f;
+    public int JumpAmount = 2;
+    public Vector3 vilocity;
+    public bool isGrounded;
+    public float isGroundedRadius=.3f;
+
+    public Vector3 gravity; 
+
     float v_move;
     float h_move;
 
+    Vector2 moveDirtion;
+
+    public InputActionAsset actionsAssests;
+    InputActionMap onFootMap;
+    InputAction moveAction;
+    InputAction jumpAction;
 
     // Start is called before the first frame update
+
+
     void Start()
     {
-        
+
+        gravity = Physics.gravity;
+
+        onFootMap = actionsAssests.FindActionMap("OnFoot");
+
+        onFootMap.Enable();
+
+        moveAction = onFootMap.FindAction("Move");
+
+        jumpAction = onFootMap.FindAction("Jump");
+
+        moveAction.performed += context => OnMove(context);
+        moveAction.canceled += ctx => OnMove(ctx);
+
+        jumpAction.performed += context => OnJump(context);
+        jumpAction.canceled += ctx => OnJump(ctx);
     }
 
     // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, isGroundedRadius);
+ 
+        
+    }
+
     void Update()
     {
-        v_move = Input.GetAxis("Vertical");
-        h_move = Input.GetAxis("Horizontal");
+        Move(moveDirtion);
         
-        Vector3 moveDir = (transform.forward* v_move)+(transform.right*h_move);
-        CharacterController.Move(moveDir * speed* Time.deltaTime);
+        if (isGrounded == true)
+        {
+            
+            vilocity.y = -2f;
+            
+
+        }
+        else
+        {
+            
+            vilocity += gravity*2*Time.deltaTime;
+        }
+        Jump();
+        CharacterController.Move(vilocity* Time.deltaTime);
+    }
+
+
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+
+        moveDirtion = context.ReadValue<Vector2>();
+    }
+
+    private void Move(Vector2 D)
+    {
+        v_move = D.y * speed * Time.deltaTime;
+        h_move = D.x * speed * Time.deltaTime;
+
+        Vector3 moveDir = (transform.forward * v_move) + (transform.right * h_move);
+        CharacterController.Move(moveDir);
+
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Jumpable = true;
+           
+        }
+        else if (context.canceled)
+        {
+            Jumpable = false;
+            
+        }
+    }
+
+    void Jump()
+    {
+        if (Jumpable)
+        {
+
+            if (isGrounded == true)
+                {
+                vilocity.y += Mathf.Sqrt(JumpHieght * -2 * gravity.y);
+                print(3);
+                }
+         
+        }
     }
 }
